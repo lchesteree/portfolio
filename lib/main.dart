742ed4f4e867c89a9b19a9cca1778a31c6ff00fa
lchesteree <lchesteree@yahoo.com.hk>
2021-06-08 01:43:23 +0800

    - improve the shadow of navbar

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,13 @@ class MyApp extends StatelessWidget {
 }
 
 class Index extends StatelessWidget {
+  final ScrollController sc = ScrollController();
+  final GlobalKey<NavigationBarState> _globalKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     bool isDesktopView = MediaQuery.of(context).size.width > 800;
+    bool isScrolledToTop = true;
 
     Map<String, Function> navigationButtonMap = {
       'EDUCATION': () => scrollToWidgetWhichKeyIs(Education.globalKey),
@@ -37,14 +41,26 @@ class Index extends StatelessWidget {
       'CONTACT': () => scrollToWidgetWhichKeyIs(Contact.globalKey),
     };
 
+    sc.addListener(() {
+      if (sc.offset == 0 && !isScrolledToTop) {
+        _globalKey.currentState
+            ?.notifyIsScrolledToTheTop(isScrolledToTop = true);
+      } else if (sc.offset != 0 && isScrolledToTop) {
+        _globalKey.currentState
+            ?.notifyIsScrolledToTheTop(isScrolledToTop = false);
+      }
+    });
+
     return Scaffold(
       body: Scrollbar(
+        controller: sc,
         thickness: 10,
         isAlwaysShown: true,
         radius: Radius.circular(8),
         child: Stack(
           children: [
             SingleChildScrollView(
+              controller: sc,
               child: Column(
                 children: [
                   const SizedBox(height: NavigationBar.height),
@@ -58,7 +74,7 @@ class Index extends StatelessWidget {
               ),
             ),
             // Navigation Bar
-            NavigationBar(isDesktopView, navigationButtonMap),
+            NavigationBar(isDesktopView, navigationButtonMap, key: _globalKey),
           ],
         ),
       ),
--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -7,27 +7,29 @@ import 'widget/navbar/navbar_brand.dart';
 
 class NavigationBar extends StatefulWidget {
   static const double height = 80;
-
   final bool isDesktopView;
   final Map<String, Function> navigationButtonMap;
 
-  NavigationBar(this.isDesktopView, this.navigationButtonMap);
+  NavigationBar(this.isDesktopView, this.navigationButtonMap, {Key? key})
+      : super(key: key);
 
   @override
-  _NavigationBarState createState() => _NavigationBarState();
+  NavigationBarState createState() => NavigationBarState();
 }
 
-class _NavigationBarState extends State<NavigationBar>
+class NavigationBarState extends State<NavigationBar>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
-  bool isOpenDropDownList = false;
-  bool isForceClose = false;
+  bool isOpenDropDownList = false,
+      isForceClose = false,
+      isScrolledToTop = true;
 
   final List<NavigationButton> buttonList = [];
 
   @override
   void initState() {
+    // Drop down list animation
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
@@ -61,26 +63,28 @@ class _NavigationBarState extends State<NavigationBar>
     );
 
     return Container(
-        height: NavigationDropDownListItem.height * 4 + NavigationBar.height,
-        child: Stack(
-          children: [
-            // Drop down list
-            Positioned(
-              left: 0,
-              right: 0,
-              top: _animation.value,
-              child: NotificationListener(
-                onNotification: (notification) {
-                  // if the size of device is changed, close the drop down list immediately
-                  if (isOpenDropDownList) {
-                    isOpenDropDownList = false;
-                    isForceClose = true;
-                    _controller.reset();
-                    isForceClose = false;
-                  }
-                  return false;
-                },
-                child: SizeChangedLayoutNotifier(
+      height: NavigationDropDownListItem.height * 4 + NavigationBar.height,
+      child: Stack(
+        children: [
+          // Drop down list
+          Positioned(
+            left: 0,
+            right: 0,
+            top: _animation.value,
+            child: NotificationListener(
+              onNotification: (notification) {
+                // if the size of device is changed, close the drop down list immediately
+                if (isOpenDropDownList) {
+                  isOpenDropDownList = false;
+                  isForceClose = true;
+                  _controller.reset();
+                  isForceClose = false;
+                }
+                return false;
+              },
+              child: SizeChangedLayoutNotifier(
+                child: Material(
+                  elevation: isOpenDropDownList ? 5 : 0,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
@@ -93,35 +97,40 @@ class _NavigationBarState extends State<NavigationBar>
                 ),
               ),
             ),
+          ),
 
-            // Navigation Bar
-            Material(
-              elevation: isOpenDropDownList ? 0 : 5,
-              child: Container(
-                height: NavigationBar.height,
-                color: Colors.white,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: widget.isDesktopView
-                      ? MainAxisAlignment.spaceAround
-                      : MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: NavBarBrand(),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 30),
-                      child: widget.isDesktopView
-                          ? navigationBar
-                          : buildDropDownButton(),
-                    ),
-                  ],
-                ),
+          // Navigation Bar
+          Material(
+            elevation: isOpenDropDownList
+                ? 0
+                : isScrolledToTop
+                    ? 0
+                    : 5,
+            child: Container(
+              height: NavigationBar.height,
+              color: Colors.white,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: widget.isDesktopView
+                    ? MainAxisAlignment.spaceAround
+                    : MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: NavBarBrand(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: widget.isDesktopView
+                        ? navigationBar
+                        : buildDropDownButton(),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -155,5 +164,11 @@ class _NavigationBarState extends State<NavigationBar>
     else
       _controller.forward();
     isOpenDropDownList = !isOpenDropDownList;
+  }
+
+  void notifyIsScrolledToTheTop(bool value) {
+    setState(() {
+      isScrolledToTop = value;
+    });
   }
 }
